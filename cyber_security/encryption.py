9a07b6251c55cd12f9c24fbdc3b8d5c8a43f95ae
Pedro <pedroandrebarrozo@gmail.com>
2023-12-26 13:43:59 -0300

    comentários adicionados

--- cyber_security/encryption.py
+++ cyber_security/encryption.py
@@ -3,16 +3,21 @@ class Ceaser:
         self.word = word
         self.n = n
         self.alfabeto = "ABCDEFGHIJKLMNOPQRSTUWXYZ"
+        
     def encryption(self):
         new_Word = ""
 
         for x in self.word:
+
+            # Teste para caracteres que nao pertencem ao alfabeto
             if x not in self.alfabeto:
                 new_Word+=x
 
+            # Teste para índices maiores do tamanho do alfabeto 
             if self.alfabeto.find(x)+self.n > len(self.alfabeto):
                 new_Word += self.alfabeto[self.alfabeto.find(x)+self.n-len(self.alfabeto)]
 
+            #
             else:
                 new_Word += self.alfabeto[self.alfabeto.find(x)+self.n]
 
@@ -24,15 +29,17 @@ class Ceaser:
 
         for x in self.word:
 
+            # Teste para caracteres que nao pertencem ao alfabeto
             if x not in self.alfabeto:
                 new_Word+=x
 
+            # Teste para índices menores que 0 
             if self.alfabeto.find(x)-self.n < 0:
                 new_Word += self.alfabeto[self.alfabeto.find(x)-self.n+len(self.alfabeto)]
 
             else:
                 new_Word += self.alfabeto[self.alfabeto.find(x)-self.n]
-                
+
         return new_Word
 
 teste = Ceaser("ZARATRUSTA", 2)
